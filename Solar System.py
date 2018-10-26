6dc4eb36e68c85762e4f1d1e2d01d3688675cab6
Roy Siu <roysiu@outlook.com>
2018-10-26 19:02:36 +0100

    Layering classes (2)
    Change some stuff
    (Amend this commit message later)
    Also:
    Break some things

--- Solar System.py
+++ Solar System.py
@@ -20,47 +20,83 @@ def set_pos(canvas, body, x, y):
     canvas.move(body, x - canvas.coords(body)[0], y - canvas.coords(body)[1])
 
 class Planet(t.Frame):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, foobaaah, x, y, name, *args, spacer=20, diff=1, borderwidth=0, **kwargs):
         super().__init__()
-        self._init_graphics(*args, **kwargs)
 
-    def _init_graphics(self):
-        pass
+        self.x = x
+        self.y = y
 
-    def set_coords(self, x, y):
+        self.name = name
+
+        self.spacer = spacer
+        self.diff = diff
+
+        self.foobaaah = foobaaah
+        self.borderwidth = borderwidth
+
+        #self._init_graphics(*args, **kwargs)
+        return args, kwargs
+
+    def _init_graphics(self, *args, **kwargs):
         pass
 
     def _create_circle(self, x, y, r, **kwargs):
         return self.canvas.create_oval(x-r, y-r, x+r, y+r, **kwargs)
 
+    def __next__(self):
+        pass
+
 class OrbSys(Planet):
     def __init__(self, *args, **kwargs):
-        super().__init__()
+        args, kwargs = super().__init__(*args, **kwargs) # OOOOOOOOHHHHHHHHHHHHHHHHH
+        print(args)
+        print(kwargs)
         self.init_graphics(*args, **kwargs)
 
-    def init_graphics(self, widget, name, \
-                        names, diameters, days_per_revolution, colours, \
-                        spacer=20, diff=1, borderwidth=0):
-        self.widget = widget
-        self.names = names
-        self.diameters = diameters
-        self.revolutions = days_per_revolution
-        self.colours = colours
+    def init_graphics(self, *args):
+        self.data = [list(arg) for arg in args]
 
-        self.borderwidth = borderwidth
+        nam_ind = 0
+        dia_ind = 1
+        rev_ind = 2
+        col_ind = 3
+
+        rad_ind = 4
+        apd_ind = 5
+        dis_ind = 6
+
+        for planet in self.data:
+            radius = planet[dia_ind] / 2
+            #self.radii = [radius / self.diff for radius in self.radii] # Smaller radii to fit on screen
+            planet.append(radius)
+
+            angles_per_day = 360 / planet[rev_ind]
+            planet.append(angles_per_day)
+
+
+        for i in self.data:
+            print(i)
+
+        self.names = [arg[0] for arg in args]
+        self.diameters = [arg[1] for arg in args]
+        self.revolutions = [arg[2] for arg in args]
+        self.colours = [arg[3] for arg in args]
+
+
 
         self.radii = [size / 2 for size in self.diameters]
-        self.radii = [size / diff for size in self.radii] # Smaller radii to fit on screen
+        #self.radii = [radius / self.diff for radius in self.radii] # Smaller radii to fit on screen
 
-        self.angles_per_day = [360 / rev for rev in self.revolutions]
+        #self.angles_per_day = [360 / rev for rev in self.revolutions]
+        #self.angles_per_day = [0]*32
 
         self.distances = [0] # First body (stationary)
         for index, radius in enumerate(self.radii[1:]): # Skip first body
             self.distances.append( self.distances[index] + self.radii[index] )
-            self.distances[index+1] += spacer + radius
+            self.distances[index+1] += self.spacer + radius
 
 
-        self.master.title(name)
+        self.master.title(self.name)
         self.pack(fill=t.BOTH, expand=1)
 
         self.canvas = t.Canvas(self)
@@ -69,24 +105,23 @@ class OrbSys(Planet):
         self.canvas.pack(fill=t.BOTH, expand=1)
 
 
-
         self.bodies = []
 
 
         self.day = 0
-        for distance, radius, colour, anglepd in zip(self.distances, self.radii, self.colours, self.angles_per_day):
-            coords = cart_pos(distance, anglepd * self.day, X_MID, Y_MID)
-            foo = self._create_circle(*coords, radius, \
-                                fill=colour, width=self.borderwidth)
+        for distance, body in zip(self.distances, self.data):
+            coords = cart_pos(distance, body[apd_ind] * self.day, self.x, self.y)
+            foo = self._create_circle(*coords, body[rad_ind], \
+                                fill=body[col_ind], width=self.borderwidth)
             self.bodies.append(foo)
-        self.widget.update()
+        self.foobaaah.update()
 
     def __next__(self):
-        for body, distance, anglepd in zip(self.bodies, self.distances, self.angles_per_day):
-            coords = cart_pos(distance, anglepd * self.day, X_MID, Y_MID)
-            #set_pos(self.canvas, body, self.canvas.coords(body)[0], self.canvas.coords(body)[1], coords[0], coords[1])
+        super().__next__()
+        for distance, body in zip(self.distances, self.bodies):
+            coords = cart_pos(distance, body[apd_ind] * self.day, x, y)
             set_pos(self.canvas, body, *coords)
-        self.widget.update()
+        self.foobaaah.update()
         self.day += 1
 
 def main():
@@ -116,10 +151,11 @@ def main():
                     "#E0D4C6"]
 
     zipped = [i for i in zip(names, diameters, revolutions, colours)]
-    print(zipped)
+    #print(zipped)
 
-    test = OrbSys(window, name="Solar System", \
-                    names=names, diameters=diameters, days_per_revolution=revolutions, colours=colours)
+    test = OrbSys(window, X_MID, Y_MID, "Solar System", \
+                    *zipped)
+    time.sleep(1)
     while True:
             next(test)
             time.sleep(.01)
