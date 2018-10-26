49e4435eed8cc44aa7358df25400d97b18a50b3a
Roy Siu <roysiu@outlook.com>
2018-10-26 20:43:03 +0100

    Rename some variables & a class Move some functionality higher up Solve incorrect orbits by using Tkinter builtin coords()

--- Solar System.py
+++ Solar System.py
@@ -8,19 +8,24 @@ X_MID = win_width / 2
 Y_MID = win_height / 2
 
 def cart_pos(rho, phi, x, y):
-    # These take radians
+    # Note: these take radians
     x = rho * math.cos( math.radians(phi) ) + x
     y = rho * math.sin( math.radians(phi) ) + y
     return x, y
 
-def set_pos(canvas, body, x, y):
-    #canvas.move(obj, cur_x-targ_x, cur_y-targ_y)
-    #canvas.move(body, targ_x-cur_x, targ_y-cur_y)
-    #self.canvas.coords(body)[1]
-    canvas.move(body, x - canvas.coords(body)[0], y - canvas.coords(body)[1])
+class Body(t.Frame):
 
-class Planet(t.Frame):
-    def __init__(self, foobaaah, x, y, name, *args, spacer=20, diff=1, borderwidth=0, **kwargs):
+    nam_ind = 0
+    rad_ind = 1
+    rev_ind = 2
+    col_ind = 3
+
+    apd_ind = 4
+    tkid_ind = 5
+
+    day = 0 # Increments in __next__()
+
+    def __init__(self, window, x, y, name, *args, spacer=20, diff=1, border_width=0, **kwargs):
         super().__init__()
 
         self.x = x
@@ -31,8 +36,10 @@ class Planet(t.Frame):
         self.spacer = spacer
         self.diff = diff
 
-        self.foobaaah = foobaaah
-        self.borderwidth = borderwidth
+        self.window = window
+        self.border_width = border_width
+
+        self.canvas = t.Canvas(self)
 
         #self._init_graphics(*args, **kwargs)
         return args, kwargs
@@ -43,89 +50,68 @@ class Planet(t.Frame):
     def _create_circle(self, x, y, r, **kwargs):
         return self.canvas.create_oval(x-r, y-r, x+r, y+r, **kwargs)
 
+    def set_pos(self, tkid, x, y):
+        x_old, y_old = self.canvas.coords(tkid)[:2]
+        x_offset = x - x_old
+        y_offset = y - y_old
+        self.canvas.move(tkid, x_offset, y_offset)
+
     def __next__(self):
-        pass
+        self.day += 1
 
-class OrbSys(Planet):
+class OrbSys(Body):
     def __init__(self, *args, **kwargs):
-        args, kwargs = super().__init__(*args, **kwargs) # OOOOOOOOHHHHHHHHHHHHHHHHH
-        print(args)
-        print(kwargs)
-        self.init_graphics(*args, **kwargs)
+        args, kwargs = super().__init__(*args, **kwargs)
 
-    def init_graphics(self, *args):
-        self.data = [list(arg) for arg in args]
+        self.bodies = [list(arg) for arg in args]
 
-        nam_ind = 0
-        dia_ind = 1
-        rev_ind = 2
-        col_ind = 3
+        for body in self.bodies:
+            body[self.rad_ind] # Smaller radii to fit on screen
 
-        rad_ind = 4
-        apd_ind = 5
-        dis_ind = 6
+            angles_per_day = 360 / body[self.rev_ind]
+            body.append(angles_per_day)
 
-        for planet in self.data:
-            radius = planet[dia_ind] / 2
-            #self.radii = [radius / self.diff for radius in self.radii] # Smaller radii to fit on screen
-            planet.append(radius)
+        self.displacements = [0] # First body (zero displacement)
+        for index, body in enumerate(self.bodies[1:]): # Skip first body
+            self.displacements.append(self.displacements[index] + self.bodies[index][self.rad_ind])
+            self.displacements[index + 1] += self.spacer + body[self.rad_ind]
 
-            angles_per_day = 360 / planet[rev_ind]
-            planet.append(angles_per_day)
-
-
-        for i in self.data:
-            print(i)
-
-        self.names = [arg[0] for arg in args]
-        self.diameters = [arg[1] for arg in args]
-        self.revolutions = [arg[2] for arg in args]
-        self.colours = [arg[3] for arg in args]
-
-
-
-        self.radii = [size / 2 for size in self.diameters]
-        #self.radii = [radius / self.diff for radius in self.radii] # Smaller radii to fit on screen
-
-        #self.angles_per_day = [360 / rev for rev in self.revolutions]
-        #self.angles_per_day = [0]*32
-
-        self.distances = [0] # First body (stationary)
-        for index, radius in enumerate(self.radii[1:]): # Skip first body
-            self.distances.append( self.distances[index] + self.radii[index] )
-            self.distances[index+1] += self.spacer + radius
-
+        print(self.displacements)
 
         self.master.title(self.name)
         self.pack(fill=t.BOTH, expand=1)
 
-        self.canvas = t.Canvas(self)
         self.canvas.config(bg='#000000')
 
         self.canvas.pack(fill=t.BOTH, expand=1)
 
+        self.init_graphics()
 
-        self.bodies = []
+    def init_graphics(self):
+        for displacement, body in zip(self.displacements, self.bodies):
+            angle = body[self.apd_ind] * self.day
+            coords = cart_pos(displacement, angle, self.x, self.y)
+            tk_id = self._create_circle(*coords, body[self.rad_ind], \
+                                        fill=body[self.col_ind], width=self.border_width)
+            body.append(tk_id)
+        self.window.update()
 
-
-        self.day = 0
-        for distance, body in zip(self.distances, self.data):
-            coords = cart_pos(distance, body[apd_ind] * self.day, self.x, self.y)
-            foo = self._create_circle(*coords, body[rad_ind], \
-                                fill=body[col_ind], width=self.borderwidth)
-            self.bodies.append(foo)
-        self.foobaaah.update()
+        #for i in self.bodies:
+        #    print(i)
 
     def __next__(self):
         super().__next__()
-        for distance, body in zip(self.distances, self.bodies):
-            coords = cart_pos(distance, body[apd_ind] * self.day, x, y)
-            set_pos(self.canvas, body, *coords)
-        self.foobaaah.update()
-        self.day += 1
+        #self.canvas.delete("all")
+        for displacement, body in zip(self.displacements, self.bodies):
+            angle = body[self.apd_ind] * self.day
+            x, y = cart_pos(displacement, angle, self.x, self.y)
+            r = body[self.rad_ind]
+            bounds_coords = (x-r, y-r, x+r, y+r) # Canvas.coords() (to move) frquires outer bounds for circles (ovals)
+            self.canvas.coords(body[self.tkid_ind], bounds_coords)
+        self.window.update()
 
 def main():
-    # name diameter rev colour
+    # name radius rev colour angles_per_day tkinter_canvas_id
     window = t.Tk()
     #window.attributes("-fullscreen", True)
     window.geometry(f"{win_width}x{win_height}")
@@ -140,17 +126,19 @@ def main():
                     5, 12, 13, 7, \
                   143, 125, 51, 50, \
                   2.3]
+    radii = [x/2 for x in diameters]
     # 1 to avoid division by 0
     revolutions = [ 1, \
                         87.97, 224.7, 365.26, 1.88 * years, \
                         11.86 * years, 29.46 * years, 84.01 * years, 164.79 * years, \
                         248.59 * years]
+    #revolutions = [360]*10
     colours = ["#FDA73E", \
                     "#89868C", "#DBD6D2", "#4958D2", "#F7835B", \
                     "#8E6549","#DBBF76","#CCF2F3","#5661FF", \
                     "#E0D4C6"]
 
-    zipped = [i for i in zip(names, diameters, revolutions, colours)]
+    zipped = [i for i in zip(names, radii, revolutions, colours)]
     #print(zipped)
 
     test = OrbSys(window, X_MID, Y_MID, "Solar System", \
@@ -162,3 +150,4 @@ def main():
     #window.mainloop()
 
 main()
+# fix private objects
